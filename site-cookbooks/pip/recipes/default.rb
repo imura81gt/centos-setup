b219487ba6d6bf7ce74d09732567c11cf3d9f055
IMURA Takashi <imura@realworld.jp>
2014-01-03 17:56:10 +0900

    pip

--- site-cookbooks/pip/recipes/default.rb
+++ site-cookbooks/pip/recipes/default.rb
@@ -7,10 +7,27 @@
 # All rights reserved - Do Not Redistribute
 #
 # Package resource
-package "python-setuptools" do
-	action :install
+=begin
+package "python-setuptools" do 
+  action :install
 end
+=end
+
+# Execute resource
+bash "curl -L https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py | python" do
+  code <<-EOH
+    curl -L https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py | python
+	EOH
+end 
+
+# Execute resource
+bash "curl -L https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python" do
+  code <<-EOH
+    curl -L https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python
+	EOH
+end 
 
 easy_install_package "pip" do
   action :install 
 end
+
